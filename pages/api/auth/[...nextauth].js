676023365b549c3203269bf70a843438fe43adf6
Mateusz Krawczyk <bulionwiniary@gmail.com>
2021-12-08 20:22:18 +0100

    add jwt-secret as env variable

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,21 +1,25 @@
 import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
+import jose from "node-jose";
+
 
 export default NextAuth({
     //configure one or more authentication providers
-    providers : [
+    providers: [
         GoogleProvider({
-            clientId : process.env.GOOGLE_CLIENT_ID,
-            clientSecret : process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
 
-    callbacks : {
-        async session({session,token}) {
+    callbacks: {
+        async session({ session, token }) {
             session.user.tag = session.user.name.split(" ").join("").toLowerCase()
 
             session.user.uid = token.sub
             return session
         }
-    }
+    },
+
+    secret: process.env.JWT_SECRET
 })
